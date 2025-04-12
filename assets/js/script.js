0d534bf6a8252c59850f7e3b83b3303b86b88c5f
Apurbo Ray <168566208+ApurboRay6@users.noreply.github.com>
2025-04-11 20:28:58 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  alert(`Username: ${username}\nPassword: ${password}`);
+});
