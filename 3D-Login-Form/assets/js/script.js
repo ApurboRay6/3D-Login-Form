e3b6aab80ea5487d7516196f06e058341722d485
Apurbo Ray <168566208+ApurboRay6@users.noreply.github.com>
2025-04-11 20:27:23 -0700

    Delete 3D-Login-Form directory

--- 3D-Login-Form/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const username = document.getElementById('username').value.trim();
-  const password = document.getElementById('password').value.trim();
-
-  alert(`Username: ${username}\nPassword: ${password}`);
-});
